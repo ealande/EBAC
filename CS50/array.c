0f24826a47f52e0b558a24af945059471bfbd352
ealande <erickalan23@gmail.com>
2022-12-26 10:29:10 -0300

    CS50 exercises in c

---
+++ CS50/array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void si (int n);
+int j = 0;
+
+int main() {
+    printf ("Type the number of lines the triangle in mario game gonna have:\n");
+    int n;
+    scanf ("%i", &n);
+    si(n);
+    return 0;
+    }
+
+void si (int n){
+    for (int i = 0; i < n; i++){
+
+            for (int j = 0; j < i + 1; j++){
+                printf ("#");
+            }
+            printf("\n");
+
+        }
+      
+  }
+
+
+    
